3a3182adc570f55dccd55a3a70d6c951c804ff42
ricocanuto <ricardomoraescanuto@gmail.com>
2023-10-12 10:54:33 -0300

    JS error - Jokenpo project

---
+++ jokenpo.js
@@ -0,0 +1,41 @@
+const result = document.querySelector('.result');
+const humanScore = document.querySelector('#human-score');
+const machineScore = document.querySelector('#machine-score');
+
+let humanScoreNumber = 0;
+let machineScoreNumber = 0;
+
+const playHuman = (humanChoice) => {
+    const machineChoice = playMachine();
+    playTheGame(humanChoice, machineChoice);
+};
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scisor'];
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    return choices[randomNumber]
+};
+
+const playTheGame = (human, machine) => {
+    console.log("Humano: " + human, "Máquina: " + machine);
+};
+
+if (human === machine) {
+    result.innerHTML = "Deu empate!";
+} else if (
+    (human === 'paper' && machine === 'rock') ||
+    (human === 'rock' && machine === 'scisor') ||
+    (human === 'scisor' && machine === 'paper')
+){
+    humanScoreNumber++;
+    humanScore.innerHTML = humanScoreNumber;
+    result.innerHTML = "Você ganhou!";
+} else {
+    machineScoreNumber++;
+    machineScore.innerHTML = machineScoreNumber;
+    result.innerHTML = "Você perdeu!";
+};
+
+
+
